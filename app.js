e04faac8d020fd651fcd6850da80e92229e73176
Brett <Bfindlay@users.noreply.github.com>
2017-09-09 13:22:03 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,95 @@
+console.log('loaded');
+// STUDENT CLICK EVENT
+
+$("#students").click(function() { 
+    console.log('clicked');
+    let a = $('.f1').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f1').css({"fill":  color});
+    console.log(a);
+  });
+
+$("#fountain").click(function() { 
+    let a = $('.f2').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#75CFF0' : '#FF0000';
+    console.log(a);
+    $('.f2').css({"fill":  color});
+    console.log(a);
+  });
+
+
+
+  $("#family").click(function() { 
+    let a = $('.f3').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f3').css({"fill":  color});
+    console.log(a);
+  });
+
+
+  $("#skate").click(function() { 
+    let a = $('.f4').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f4').css({"fill":  color});
+    console.log(a);
+  });
+
+
+  $("#wedding").click(function() { 
+    let a = $('.f5').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f5').css({"fill":  color});
+    console.log(a);
+  });
+
+
+  $("#buskers").click(function() { 
+    let a = $('.f6').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f6').css({"fill":  color});
+    console.log(a);
+  });
+
+
+  $("#chess").click(function() { 
+    let a = $('.f7').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f7').css({"fill":  color});
+    console.log(a);
+  });
+
+
+  $("#dancing").click(function() { 
+    let a = $('.f8').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f8').css({"fill":  color});
+    console.log(a);
+  });
+
+
+$("#homeless").click(function() { 
+    let a = $('.f9').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f9').css({"fill":  color});
+    console.log(a);
+  });
+
+  $("#tourist").click(function() { 
+    let a = $('.f10').css('fill');
+    let color =  (a === 'rgb(255, 0, 0)') ? '#B6E59E' : '#FF0000';
+    console.log(a);
+    $('.f10').css({"fill":  color});
+    console.log(a);
+  });
+
+
+
+  
